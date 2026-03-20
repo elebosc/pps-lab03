928faabc9fb85aaf9d31a8ae0f5b51dedae3d7ef
Elena Boschetti <elena.boschetti@studio.unibo.it>
2026-03-20 18:37:43 +0100

    [t2, 2+3] add tests and implementation for foldLeft and getDistinctCoursesNumber

--- src/main/scala/it/unibo/pps/task2/Task2.scala
+++ src/main/scala/it/unibo/pps/task2/Task2.scala
@@ -1,11 +1,27 @@
 package it.unibo.pps.task2
 
 import it.unibo.pps.task1.Sequences.*
-import it.unibo.pps.task1.Sequences.Sequence.{filter, flatMap, map}
+import Sequence.*
 import it.unibo.pps.task2.People.Person
 import it.unibo.pps.task2.People.Person.{course, isTeacher}
+
+import scala.annotation.tailrec
 
 object Task2:
   
   def getTeachersCourses(s: Sequence[Person]): Sequence[String] =
     map(filter(s)(p => isTeacher(p)))(p => course(p))
+
+  def getDistinctCoursesNumber(s: Sequence[Person]): Int =
+    @tailrec
+    def _countItems(s: Sequence[String])(acc: Int): Int = s match
+      case Nil() => acc
+      case Cons(_, t) => _countItems(t)(acc + 1)
+    _countItems(distinct(getTeachersCourses(s)))(0)
+
+  def foldLeft(s: Sequence[Int])(k: Int)(f: (Int, Int) => Int): Int =
+    def _foldLeft(s: Sequence[Int]): Int = s match
+      case Nil() => k
+      case Cons(h, Nil()) => f(k, h)
+      case Cons(h, t) => f(_foldLeft(t), h)
+    _foldLeft(reverse(s))
--- src/test/scala/it/unibo/pps/task2/Task2Test.scala
+++ src/test/scala/it/unibo/pps/task2/Task2Test.scala
@@ -1,15 +1,14 @@
 package it.unibo.pps.task2
 
+import it.unibo.pps.task1.Sequences.*
+import Sequence.*
 import it.unibo.pps.task2.People.Person
 import it.unibo.pps.task2.People.Person.{Student, Teacher}
-import it.unibo.pps.task2.Task2.getTeachersCourses
+import it.unibo.pps.task2.Task2.{foldLeft, getDistinctCoursesNumber, getTeachersCourses}
 import org.junit.Test
 import org.junit.Assert.assertEquals
 
 class Task2Test:
-
-  import it.unibo.pps.task1.Sequences.*
-  import Sequence.*
   
   @Test def testFilterOnEmptyListReturnsEmptyList(): Unit =
     val sequence: Sequence[Person] = Nil()
@@ -26,5 +25,57 @@ class Task2Test:
     val sequence = Cons(student1, Cons(teacher1, Cons(teacher2, Cons(student2, Nil()))))
     val expectedSequence = Cons(course1, Cons(course2, Nil()))
     assertEquals(expectedSequence, getTeachersCourses(sequence))
+
+  @Test def testDistinctCoursesAreCountedCorrectlyOnNonEmptySequence(): Unit =
+    val teacher1 = Teacher("Viroli", "PPS")
+    val teacher2 = Teacher("Aguzzi", "PPS")
+    val teacher3 = Teacher("Ricci", "PCD")
+    val sequence = Cons(teacher1, Cons(teacher2, Cons(teacher3, Nil())))
+    val expectedCoursesNumber = 2
+    assertEquals(expectedCoursesNumber, getDistinctCoursesNumber(sequence))
+
+  @Test def testDistinctCoursesReturnsZeroOnEmptySequence(): Unit =
+    val sequence: Sequence[Person] = Nil()
+    val expectedCoursesNumber = 0
+    assertEquals(expectedCoursesNumber, getDistinctCoursesNumber(sequence))
     
-    
+  @Test def testFoldLeftReturnsCorrectResultOnNonEmptySequenceAndNullDefaultValue(): Unit =
+    val sequence = Cons(3, Cons(7, Cons(1, Cons(5, Nil()))))
+    val defaultValue = 0
+    val function: (Int, Int) => Int = _ - _
+    val result = foldLeft(sequence)(defaultValue)(function)
+    val expectedResult = -16
+    assertEquals(expectedResult, result)
+
+  @Test def testFoldLeftReturnsCorrectResultOnNonEmptySequenceAndPositiveDefaultValue(): Unit =
+    val sequence = Cons(3, Cons(7, Cons(1, Cons(5, Nil()))))
+    val defaultValue = 3
+    val function: (Int, Int) => Int = _ - _
+    val result = foldLeft(sequence)(defaultValue)(function)
+    val expectedResult = -13
+    assertEquals(expectedResult, result)
+
+  @Test def testFoldLeftReturnsCorrectResultOnNonEmptySequenceAndNegativeDefaultValue(): Unit =
+    val sequence = Cons(3, Cons(7, Cons(1, Cons(5, Nil()))))
+    val defaultValue = -3
+    val function: (Int, Int) => Int = _ - _
+    val result = foldLeft(sequence)(defaultValue)(function)
+    val expectedResult = -19
+    assertEquals(expectedResult, result)
+
+  @Test def testFoldLeftReturnsCorrectResultOnNonEmptySequenceWithNegativeValues(): Unit =
+    val sequence = Cons(3, Cons(-7, Cons(1, Cons(-5, Nil()))))
+    val defaultValue = 0
+    val function: (Int, Int) => Int = _ - _
+    val result = foldLeft(sequence)(defaultValue)(function)
+    val expectedResult = 8
+    assertEquals(expectedResult, result)
+
+  @Test def testFoldLeftReturnsDefaultValueOnEmptySequence(): Unit =
+    val sequence: Sequence[Int] = Nil()
+    val defaultValue = 3
+    val function: (Int, Int) => Int = _ - _
+    val result = foldLeft(sequence)(defaultValue)(function)
+    val expectedResult = defaultValue
+    assertEquals(expectedResult, result)
+
